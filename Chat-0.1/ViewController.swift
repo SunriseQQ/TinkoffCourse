94beb5dadde128e10a46ede1a79be95de1d4f9cf
SunriseQQ <sunriseqq99@gmail.com>
2020-02-13 14:09:45 +0300

    Initial Commit

---
+++ Chat-0.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat-0.1
+//
+//  Created by Sunrise on 13.02.2020.
+//  Copyright © 2020 Sunrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
