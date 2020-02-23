78df3969317cb3219ca054660a2e4437f6425f7d
SunriseQQ <sunriseqq99@gmail.com>
2020-02-23 04:04:33 +0300

    HW 2 completed

--- Chat-0.1/SceneDelegate.swift
+++ Chat-0.1/SceneDelegate.swift
@@ -11,74 +11,55 @@ import UIKit
 
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     @available(iOS 13.0, *)
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        if doYouWannaHaveLogs {
-            print("Application moved from <Non running> to <Inactive>: \(#function)")
-        }
-        
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
     @available(iOS 13.0, *)
     func sceneDidDisconnect(_ scene: UIScene) {
-        if doYouWannaHaveLogs {
-            print("Application moved from <Background> to <Suspended>: \(#function)")
-        }
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
-        print(#function)
+        
     }
-
+    
     @available(iOS 13.0, *)
     func sceneDidBecomeActive(_ scene: UIScene) {
-        if doYouWannaHaveLogs {
-            print("Application moved from <Inactive> to <Active>: \(#function)")
-        }
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     @available(iOS 13.0, *)
     func sceneWillResignActive(_ scene: UIScene) {
-        if doYouWannaHaveLogs {
-            print("Application moved from <Active> to <Inactive>: \(#function)")
-        }
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     @available(iOS 13.0, *)
     func sceneWillEnterForeground(_ scene: UIScene) {
-        if doYouWannaHaveLogs {
-            print("Application moved from <Background> to <Inactive>: \(#function)")
-        }
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     @available(iOS 13.0, *)
     func sceneDidEnterBackground(_ scene: UIScene) {
-        if doYouWannaHaveLogs {
-            print("Application moved from <Inactive> to <Background>: \(#function)")
-        }
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-
+        
         // Save changes in the application's managed object context when the application transitions to the background.
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
-
-
+    
+    
 }
 
--- Chat-0.1/ViewController.swift
+++ Chat-0.1/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var informationLabel: UILabel!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // print(editButton.frame)
+        //Причина ошибки попытка вывести значения frame до полной инициализации editButton.
+    }
+
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         profileImageButton.backgroundColor = UIColor(hex: "#3F78F0FF")
@@ -26,8 +36,20 @@ class ViewController: UIViewController {
         editButton.layer.borderWidth = 0.75
         
         profileImage.layer.cornerRadius = profileImageButton.frame.size.height / 2
-
-
+        
+        print(editButton.frame)
+        //Показывается значения frame выгруженные из .storyboard файла
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(editButton.frame)
+        /* Причина различия свойства frame от frame в viewDidLoad заключается в том что:
+         в отличии от значений frame загруженных
+         из .storyboard, в методе viewDidAppear свойства UIButton
+         и остальных компонентов уже измененны в
+         соответсвии с AutoLayout для устройства указанного в симуляторе. */
     }
     
     override func viewDidLayoutSubviews() {
@@ -37,9 +59,9 @@ class ViewController: UIViewController {
         informationLabel.frame.size.width = UIScreen.main.bounds.width * 0.9
         informationLabel.frame.size.height = UIScreen.main.bounds.width * 0.01
         informationLabel.sizeToFit()
-               informationLabel.text = "qwertyuiop[asdfghjkl;zxcvbnmcsdfghjfdsfghjgfdsfghjfdsdhjkgfdsf,sdfgfhjgkhvfghjkhgfdghj."
-    }
         
+    }
+    
     func showImagePickerControllerActionSheet() {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         let choosePhotoButton = UIAlertAction(title: "Choose from Library", style: .default) {action in
@@ -65,27 +87,27 @@ class ViewController: UIViewController {
 extension UIColor {
     public convenience init?(hex: String) {
         let r, g, b, a: CGFloat
-
+        
         if hex.hasPrefix("#") {
             let start = hex.index(hex.startIndex, offsetBy: 1)
             let hexColor = String(hex[start...])
-
+            
             if hexColor.count == 8 {
                 let scanner = Scanner(string: hexColor)
                 var hexNumber: UInt64 = 0
-
+                
                 if scanner.scanHexInt64(&hexNumber) {
                     r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
                     g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
                     b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
                     a = CGFloat(hexNumber & 0x000000ff) / 255
-
+                    
                     self.init(red: r, green: g, blue: b, alpha: a)
                     return
                 }
             }
         }
-
+        
         return nil
     }
 }
