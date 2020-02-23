34fe58ab3cb9ccd254ea25b217c51a87fe90c033
SunriseQQ <37340951+SunriseQQ@users.noreply.github.com>
2020-02-23 06:08:58 +0300

    HW№2
    * HW#2 completed
    Complete profile view app with image load

--- Chat-0.1/ViewController.swift
+++ Chat-0.1/ViewController.swift
@@ -10,50 +10,116 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileImageButton: UIButton!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var informationLabel: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // print(editButton.frame)
+        //Причина ошибки попытка вывести значения frame до полной инициализации editButton.
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        profileImageButton.backgroundColor = UIColor(hex: "#3F78F0FF")
+        profileImageButton.layer.cornerRadius = profileImageButton.frame.size.height / 2
+        
+        editButton.layer.cornerRadius = 10
+        editButton.layer.borderColor = UIColor.black.cgColor
+        editButton.layer.borderWidth = 0.75
+        
+        profileImage.layer.cornerRadius = profileImageButton.frame.size.height / 2
+        
+        print(editButton.frame)
+        //Показывается значения frame выгруженные из .storyboard файла
+        
     }
+    
+    
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-        if doYouWannaHaveLogs {
-            print("ViewController visible state moved from <Appearing> to <Appeared>: \(#function)")
-        }
+        print(editButton.frame)
+        /* Причина различия свойства frame от frame в viewDidLoad заключается в том что:
+         в отличии от значений frame загруженных
+         из .storyboard, в методе viewDidAppear свойства UIButton
+         и остальных компонентов уже измененны в
+         соответсвии с AutoLayout для устройства указанного в симуляторе. */
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        if doYouWannaHaveLogs {
-            print("ViewController visible state moved from <Disappeared> to <Appearing>: \(#function)")
-        }
-     }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        if doYouWannaHaveLogs {
-            print("View will adjust the position of its subviews: \(#function)")
-        }
-    }
-
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if doYouWannaHaveLogs {
-            print("View adjusted the position of its subviews: \(#function)")
-        }
+        
+        informationLabel.frame.size.width = UIScreen.main.bounds.width * 0.9
+        informationLabel.frame.size.height = UIScreen.main.bounds.height * 0.5
+        informationLabel.sizeToFit()
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(true)
-        if doYouWannaHaveLogs {
-            print("ViewController visible state moved from <Disappearing> to <Disapeared>: \(#function)")
+    func showImagePickerControllerActionSheet() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let choosePhotoButton = UIAlertAction(title: "Choose from Library", style: .default) {action in
+            self.showImagePickerController(sourceType: .photoLibrary)
         }
+        let takePhotoButton = UIAlertAction(title: "Take from Camera", style: .default) {action in
+            self.showImagePickerController(sourceType: .camera)
+        }
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(choosePhotoButton)
+        alert.addAction(takePhotoButton)
+        alert.addAction(cancelButton)
+        present(alert, animated: true, completion: nil)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        if doYouWannaHaveLogs {
-            print("ViewController visible state moved from <Appeared> to <Disappearing>: \(#function)")
+    @IBAction func profileImageButtonAction(_ sender: Any) {
+        showImagePickerControllerActionSheet()
+    }
+    
+}
+
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+        
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+            
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
         }
+        
+        return nil
     }
 }
 
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            profileImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func showImagePickerController(sourceType: UIImagePickerController.SourceType){
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = sourceType
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+}
