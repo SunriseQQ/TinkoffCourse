302e644276f7335ce66260cc39cf98bb7d614191
SunriseQQ <sunriseqq99@gmail.com>
2020-03-01 21:21:49 +0300

    fix

--- Chat-0.1/Model/AppDelegate.swift
+++ Chat-0.1/Model/AppDelegate.swift
@@ -14,20 +14,11 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-<<<<<<< HEAD:Chat-0.1/AppDelegate.swift
-    
-=======
-<<<<<<< HEAD:Chat-0.1/Model/AppDelegate.swift
-<<<<<<< HEAD:Chat-0.1/Model/AppDelegate.swift
+
     var navigationController: UINavigationController?
-=======
+
     
->>>>>>> master:Chat-0.1/AppDelegate.swift
-=======
-    
->>>>>>> master:Chat-0.1/AppDelegate.swift
->>>>>>> checkThirdHW:Chat-0.1/Model/AppDelegate.swift
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //               window = UIWindow(frame: UIScreen.main.bounds)
         //
