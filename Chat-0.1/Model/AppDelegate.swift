bb7d31f4e5785b3f70fdeefd2777b729035914f0
SunriseQQ <sunriseqq99@gmail.com>
2020-03-01 21:35:27 +0300

    fix

--- Chat-0.1/Model/AppDelegate.swift
+++ Chat-0.1/Model/AppDelegate.swift
@@ -14,11 +14,8 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-
     var navigationController: UINavigationController?
-
     
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //               window = UIWindow(frame: UIScreen.main.bounds)
         //
